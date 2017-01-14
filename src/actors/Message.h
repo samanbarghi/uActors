18e0b260a0108ac3646c425c7f7b7e7aebcfcf41
Saman Barghi <saman.b@gmail.com>
2017-01-14 16:24:07 -0500

    first commit

---
+++ src/actors/Message.h
@@ -0,0 +1,21 @@
+/*
+ * Message.h
+ *
+ *  Created on: Jan 14, 2017
+ *      Author: saman
+ */
+
+#ifndef SRC_ACTORS_MESSAGE_H_
+#define SRC_ACTORS_MESSAGE_H_
+
+
+namespace uActors {
+
+class Message{
+
+};
+
+}  // namespace uActors
+
+
+#endif /* SRC_ACTORS_MESSAGE_H_ */
